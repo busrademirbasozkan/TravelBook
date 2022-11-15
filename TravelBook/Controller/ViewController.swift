68fb5ecfb1de73a8e6b98d2681fb49441fa0c297
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-15 16:01:10 +0300

    Kullanıcı konumu alabilmek için ilgili kütüphaneler, protokoller eklendi.

--- TravelBook/Controller/ViewController.swift
+++ TravelBook/Controller/ViewController.swift
@@ -7,18 +7,26 @@
 
 import UIKit
 import MapKit
+import CoreLocation // kullanıcının konumunu alabilmek için ilgili kütüphane
 
-class ViewController: UIViewController, MKMapViewDelegate{
+class ViewController: UIViewController, MKMapViewDelegate , CLLocationManagerDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // locationı ne kadar doğrulukla görüntülemesini sağlıyoruz.
+        locationManager.requestWhenInUseAuthorization() // kullanıcının konumuna ne zaman ulaşacağımızı seçmeliyiz.
+        locationManager.startUpdatingLocation() // kullanıcı konumu bu komut ile alınmaya başlanıyor
         
     }
-
+    
+    
+   
 
 }
 
